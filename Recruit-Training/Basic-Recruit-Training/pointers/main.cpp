2f4a6bbc17a6b79583c1d936e8f077ad0960f7a5
Tyler <tylertran1017@gmail.com>
2021-10-23 10:55:49 -0600

    Completed pointers training

--- Recruit-Training/Basic-Recruit-Training/pointers/main.cpp
+++ Recruit-Training/Basic-Recruit-Training/pointers/main.cpp
@@ -12,6 +12,8 @@ using namespace std;
 void foo(int *a, int b);
 void bar(int *a, int b);
 
+void mySwap(int* a, int* b);
+
 int main()
 {
     int x = 3;
@@ -19,27 +21,30 @@ int main()
     int *p = &x;
 
     //TODO in the line below predict what what is going to be output
-    cout << "Predicted value of p: " /* << PLACE YOUR PREDICTION HERE*/ << endl;
+    cout << "Predicted value of p: " << "ADDRESS OF X" << endl;
     cout << "Actual value of p: " << p << endl;
-    cout << "Predicted value &x: " /* << PLACE YOUR PREDICTION HERE*/ << endl;
+    cout << "Predicted value &x: " << "ADDRESS OF X" << endl;
     cout << "Actual value &x: " << &x << endl;
-    cout << "Predicted value of *p: " /* << PLACE YOUR PREDICTION HERE*/ << endl;
+    cout << "Predicted value of *p: " << 3 << endl;
     cout << "Actual value of *p: " << *p << endl;
     
     foo(p, x);
     
-    cout << "Predicted value of *p: " /* << PLACE YOUR PREDICTION HERE*/ << endl;
+    cout << "Predicted value of *p: " << 42 << endl;
     cout << "Actual value of *p: " << *p << endl;
-    cout << "Predicted value of x: " /* << PLACE YOUR PREDICTION HERE*/ << endl;
+    cout << "Predicted value of x: " << 42 << endl;
     cout << "Actual value of x: " << x << endl;
     foo(p, y);
 
-    cout << "Predicted value of *p: " /* << PLACE YOUR PREDICTION HERE*/ << endl;
+    cout << "Predicted value of *p: " << 42 << endl;
     cout << "Actual value of *p: " << *p << endl;
-    cout << "Predicted value of y: " /* << PLACE YOUR PREDICTION HERE*/ << endl;
+    cout << "Predicted value of y: " << 4 << endl;
     cout << "Actual value of y: " << y << endl;
 
     //start writing mySwap here
+    mySwap(&x,&y);
+    cout << "Value of x: " << x << endl;
+    cout << "Value od y: " << y << endl;
     
     return 0;
 }
@@ -54,4 +59,11 @@ void var(int *a, int b)
 {
     *a = 365;
     b = 912;
+}
+
+void mySwap(int* a, int* b)
+{
+    *a = *a xor *b;
+    *b = *b xor *a;
+    *a = *a xor *b;
 }
