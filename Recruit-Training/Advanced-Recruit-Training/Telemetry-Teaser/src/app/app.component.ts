4bb722ffdf3021bb1a95b5271563bd2377667ce7
brian-ngyn <brian.nguyen3@ucalgary.ca>
2021-10-30 15:45:17 -0600

    Edited app.component.html and app.component.ts

--- Recruit-Training/Advanced-Recruit-Training/Telemetry-Teaser/src/app/app.component.ts
+++ Recruit-Training/Advanced-Recruit-Training/Telemetry-Teaser/src/app/app.component.ts
@@ -23,12 +23,11 @@ export class AppComponent {
   constructor() {
     this.title = 'Telemetry Recruit Training';
     this.weather = 50;
-    this.weather = 0;
     this.speed = 0;
     this.battery = 0;
     this.speedInput = new FormControl(null, [Validators.required, Validators.min(0), Validators.max(90)]);
     this.batteryInput = new FormControl(null, [Validators.required, Validators.min(0), Validators.max(100)]);
-    this.outputMessage = "";
+    this.outputMessage = '';
     this.buttonPress = false;
   }
 
@@ -49,7 +48,9 @@ export class AppComponent {
 
   calculateRange() {
     this.buttonPress = true;
+    console.log(this.speed);
+    console.log(this.battery);
     this.range = -(this.speed * this.speed * this.battery / 2500) + (4 * this.battery) + this.weather;
-    this.outputMessage = "The predicted range of the Elysia is " + this.range + "km.";
+    this.outputMessage = 'The predicted range of the Elysia is ' + this.range + ' km.';
   }
-};
+}
