b90d32e4eb8a5503692b924ba6de084fe25aefbd
haley <haley.cummings@ucalgary.ca>
2021-11-04 21:06:59 -0600

    completed pointers

--- Recruit-Training/Basic-Recruit-Training/pointers/main.cpp
+++ Recruit-Training/Basic-Recruit-Training/pointers/main.cpp
@@ -12,6 +12,14 @@ using namespace std;
 void foo(int *a, int b);
 void bar(int *a, int b);
 
+void mySwap(int *a, int *b)
+{
+     int hold = *a;
+     *a = *b;
+     *b = hold;
+
+}
+
 int main()
 {
     int x = 3;
@@ -19,28 +27,32 @@ int main()
     int *p = &x;
 
     //TODO in the line below predict what what is going to be output
-    cout << "Predicted value of p: " /* << PLACE YOUR PREDICTION HERE*/ << endl;
+    cout << "Predicted value of p: memory of x " /* << PLACE YOUR PREDICTION HERE*/ << endl;
     cout << "Actual value of p: " << p << endl;
-    cout << "Predicted value &x: " /* << PLACE YOUR PREDICTION HERE*/ << endl;
+    cout << "Predicted value &x: memory of x" /* << PLACE YOUR PREDICTION HERE*/ << endl;
     cout << "Actual value &x: " << &x << endl;
-    cout << "Predicted value of *p: " /* << PLACE YOUR PREDICTION HERE*/ << endl;
+    cout << "Predicted value of *p: 3" /* << PLACE YOUR PREDICTION HERE*/ << endl;
     cout << "Actual value of *p: " << *p << endl;
     
     foo(p, x);
     
-    cout << "Predicted value of *p: " /* << PLACE YOUR PREDICTION HERE*/ << endl;
+    cout << "Predicted value of *p: 42" /* << PLACE YOUR PREDICTION HERE*/ << endl;
     cout << "Actual value of *p: " << *p << endl;
-    cout << "Predicted value of x: " /* << PLACE YOUR PREDICTION HERE*/ << endl;
+    cout << "Predicted value of x: 78 " /* << PLACE YOUR PREDICTION HERE*/ << endl;
     cout << "Actual value of x: " << x << endl;
     foo(p, y);
 
-    cout << "Predicted value of *p: " /* << PLACE YOUR PREDICTION HERE*/ << endl;
+    cout << "Predicted value of *p: 42" /* << PLACE YOUR PREDICTION HERE*/ << endl;
     cout << "Actual value of *p: " << *p << endl;
-    cout << "Predicted value of y: " /* << PLACE YOUR PREDICTION HERE*/ << endl;
+    cout << "Predicted value of y: 4" /* << PLACE YOUR PREDICTION HERE*/ << endl;
     cout << "Actual value of y: " << y << endl;
 
     //start writing mySwap here
-    
+    mySwap(p, &y);
+
+    cout << "The value of p is " << *p << endl;
+    cout << "The value of y is " << y << endl;
+
     return 0;
 }
 
