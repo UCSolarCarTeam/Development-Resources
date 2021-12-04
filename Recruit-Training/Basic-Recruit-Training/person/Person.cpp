479dda53bdf4809631ade21a08a52134be5760a9
ri-yad-a <riyadabdullayev04@gmail.com>
2021-12-04 10:54:27 -0700

    Fixed issues with Basic Recruit Training

--- Recruit-Training/Basic-Recruit-Training/person/Person.cpp
+++ Recruit-Training/Basic-Recruit-Training/person/Person.cpp
@@ -1,43 +1,37 @@
 #include <iostream> 
 #include "Person.h"
+#include <string>
 
-
-
-
-std::string(n);
+std::string n;
 int a;
 
 Person::Person() 
 {
-
+	name_ = "";
+	age_ = new int(0);
 }
 
 Person::~Person() 
 {
-
+	free(age_);
 }
 
 Person::Person(const std::string& name, int age) 
 {
 	name_ = name;
 	age_ = new int(age);
-
 } 
 
 // getters and setters
 int Person::getAge() const 
 { 
-
 	return *age_;
-
 }
 
 
 const std::string& Person::getName() const 
 {
-
 	return name_;
-
 }
 
 void Person::setName(const std::string& newName)
@@ -48,32 +42,24 @@ void Person::setName(const std::string& newName)
 	
 void Person::setAge(int newAge) 
 {
-
 	*age_ = newAge;
-
 }
 
 // Print out the name and age of the person
 void Person::printInfo() const 
 {
-
 	std::cout << "Name: " << name_ << ", Age: " << *age_ << std::endl;
-
 }
 
 // Print the combined age of everyone pointed to by personArray
 int Person::combinedAge(Person** personArray, int size) 
 {
-
 	int ageTotal = 0;
 	for (int i = 0; i < size; i++) {
 		ageTotal = ageTotal + personArray[i]->getAge();
 	}
 
 	return ageTotal;
-
-
-
 }
 
 // Increases the Person's Age by 1
@@ -81,6 +67,3 @@ void Person::birthday(Person& x)
 {
 	x.setAge(x.getAge() + 1);
 }
-
-
-
