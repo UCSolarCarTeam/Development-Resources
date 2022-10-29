b86eca8917a359df9ee0914d28e8db0da901ca63
Marcelo Li Gonzales <marcelo.ligonzales@ucalgary.ca>
2022-10-29 10:37:27 -0600

    succesfully sends a heartbeat can message

--- Recruit-Training/Embedded-Recruit-Training/Core/Src/SendHeartbeatTask.c
+++ Recruit-Training/Embedded-Recruit-Training/Core/Src/SendHeartbeatTask.c
@@ -13,29 +13,25 @@ void sendHeartbeatTask(void const* arg)
 
 void sendHeartbeat(uint32_t* prevWakeTimePtr)
 {
-    osDelayUntil(prevWakeTimePtr, LIGHTS_HEARTBEAT_FREQ);
-    // Allocate CAN Message, deallocated by sender "sendCanTask()"
-    CanMsg* msg = {0};
-    /// Populate CAN Message
-    msg->StdId = LIGHTS_HEARTBEAT_STDID;
-    msg->DLC = 1;
-    msg->Data[0] = 1;
 
+    osDelay(LIGHTS_HEARTBEAT_FREQ);
+    *prevWakeTimePtr += LIGHTS_HEARTBEAT_FREQ;
 
     // Send CAN Message
 
         CAN_TxHeaderTypeDef hdr = {0};
-        hdr.StdId = msg->StdId;
-        hdr.DLC = msg->DLC;
+        hdr.StdId = 0x20U;
+        hdr.DLC = 1U;
+        uint8_t data[1] = {2};
         if (HAL_CAN_GetTxMailboxesFreeLevel(&hcan2) > 0)
     {
         uint32_t mailbox;
 
-        if (HAL_CAN_AddTxMessage(&hcan2, &(hdr), msg->Data, &mailbox) == HAL_OK)
+        if (HAL_CAN_AddTxMessage(&hcan2, &(hdr), data, &mailbox) == HAL_OK)
         {
             HAL_GPIO_TogglePin(LED_GREEN_GPIO_Port, LED_GREEN_Pin);
         }
-        else 
+        else
         {
             HAL_CAN_Init(&hcan2);
         }
