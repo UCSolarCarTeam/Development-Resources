511ecb32b956b2132b29f2cc7775a6e93620ea44
Mason <mason.bullock@ucalgary.ca>
2022-11-25 16:40:36 -0700

    completed telemetry teaser

--- Recruit-Training/Advanced-Recruit-Training/Telemetry-Teaser/src/app/app.component.ts
+++ Recruit-Training/Advanced-Recruit-Training/Telemetry-Teaser/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
@@ -13,20 +14,27 @@ export class AppComponent {
   weather = 50;
   speed = 0;
   battery = 0;
+  clicked = false;
+  carRange = 0;
 
   onMouseWeather(event: MouseEvent) {
     this.weather = parseInt((event.target as HTMLInputElement).value);
+    this.clicked = false;
   }
 
   onKeySpeed(event: KeyboardEvent) {
     this.speed = parseInt((event.target as HTMLInputElement).value);
+    this.clicked = false;
   }
 
   onKeyBattery(event: KeyboardEvent) {
     this.battery = parseInt((event.target as HTMLInputElement).value);
+    this.clicked = false;
   }
 
   calculateRange() {
-
+    this.clicked = true;
+    this.carRange = -((this.speed)^2 * this.battery / 2500) + 4 * this.battery + this.weather;
+    //console.log("Weather: " + this.weather + "\nSpeed: " + this.speed + "\nBattery: " + this.battery + "\nRange: " + this.carRange);
   }
 }
