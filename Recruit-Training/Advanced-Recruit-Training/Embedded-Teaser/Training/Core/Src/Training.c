2690ee02afcba8cc4952eb71f8e925cc52e1d772
brian nguyen <83795923+brian-ngyn@users.noreply.github.com>
2023-01-12 18:42:31 -0700

    Revert "complete basic training"

--- Recruit-Training/Advanced-Recruit-Training/Embedded-Teaser/Training/Core/Src/Training.c
+++ Recruit-Training/Advanced-Recruit-Training/Embedded-Teaser/Training/Core/Src/Training.c
@@ -1,65 +1,6 @@
 #include "Training.h"
-#include <stdio.h>
 
-void trainingTask(uint8_t* data){
-    uint8_t motor1_forward_or_reverse = data[0] & 0b00000001;
-    uint8_t motor_1_velocity = data[0] & 0b00111110;
-    uint8_t motor_1_sign = data[0] & 0b01000000;
-    uint8_t motor_1_on_off = data[0] & 0b10000000;
-    uint8_t motor2_forward_or_reverse = data[1] & 0b00000001;
-    uint8_t motor_2_velocity = data[1] & 0b00111110;
-    uint8_t motor_2_sign = data[1] & 0b01000000;
-    uint8_t motor_2_on_off = data[1] & 0b10000000;
-    uint8_t headlights_off = data[2] & 0b00000001; 
-    uint8_t headlights_low = data[2] & 0b00000010; 
-    uint8_t headlights_high = data[2] & 0b00000100;
-    uint8_t right_signal = data[2] & 0b00001000;
-    uint8_t left_signal = data[2] & 0b00010000;
-    uint8_t hazards = data[2] & 0b00100000;
-    uint8_t brakes = data[2] & 0b01000000;
-    
-    validData = 0b00000111;
-    outputArray[0] = 0b00000001;
-    outputArray[1] = 0b00000010;
-    outputArray[2] = 0b00000100;
-    //VALID IF:
-    
-    //Both motors must be in same direction, ON or OFF, M1 Velocity == M2 Velocity 
-    if(
-        motor1_forward_or_reverse != motor2_forward_or_reverse ||
-        motor_1_on_off != motor_2_on_off ||
-        motor_1_velocity != motor_2_velocity
-    ){
-        outputArray[0] = 0b00000000;
-        validData &= 0b00000100;
-        outputArray[1] = 0b00000000;
-    }
-    //Motor1 velocity is + and in forward dir.
-    //Motor1 velocity is - and in reverse dir.
-    if(
-        (motor1_forward_or_reverse == 0b00000001 && motor_1_sign > 0) ||
-        ((motor1_forward_or_reverse == 0b00000000 && motor_1_sign == 0 && motor_1_velocity != 0))
-    ){
-        outputArray[0] = 0b00000000;
-        validData &= 0b00000110;
-    }
-    
-    //same as above mtr 2.
-    if(
-        (motor2_forward_or_reverse == 0b00000001 && motor_2_velocity < 0) ||
-        ((motor2_forward_or_reverse == 0b00000000 && motor_2_sign == 0) && (motor_2_velocity != 0))
-    ){
-        outputArray[1] = 0b00000000;
-        validData &= 0b00000101;        
-    }
-    
-    //Headlights off = 1 and low or high == 0
-    //left/right signals ON when hazards ON
-    if(
-    (headlights_off == 0b00000001 && (headlights_low == 0b00000010 || headlights_high == 0b00000100)) ||
-    (hazards == 0b00000000 && right_signal == 0b00001000 && left_signal == 0b00010000)
-    ){
-        outputArray[2] = 0b00000000;
-        validData &= 0b00000011;        
-    }
+void trainingTask(uint8_t* data)
+{
+
 }
--- Recruit-Training/Basic-Recruit-Training/person/main.cpp
+++ Recruit-Training/Basic-Recruit-Training/person/main.cpp
@@ -20,18 +20,14 @@ void compareAnswers(const std::string myAnswer, const std::string correctAnswer)
 // Swaps a & b.
 void mySwap(int& a, int& b)
 {
-    int temp_a = a;
-    a = b;
-    b = temp_a;
 }
 // Increment the int that x is pointing to by one.
-void myIncrement(int& x)
+void myIncrement(int* x)
 {
-    x++;
 }
 
 // Uncomment this when person.cpp has been created.
-void personFunctions()
+/* void personFunctions()
 {
     Person Bill;
     Bill.setName("Bill");
@@ -62,7 +58,7 @@ void personFunctions()
               << myBirthdayAnswer.str() << std::endl;
     compareAnswers(myBirthdayAnswer.str(), correctBirthdayAnswer);
     delete Joe;
-}
+} */
 
 int main()
 {
@@ -70,9 +66,8 @@ int main()
     int b = 6;
 
     // Pass a & b into mySwap here
-    mySwap(a,b);
     // Pass in a to myIncrement here
-    myIncrement(a);
+
     std::cout << "Checking Swap and Increment: " << std::endl;
 
     std::stringstream swapIncrementAnswer;
@@ -82,10 +77,9 @@ int main()
 
     compareAnswers(swapIncrementAnswer.str(), correctSwapIncrementAnswer);
     // Pass a and b into mySwap
-    mySwap(a,b);
     // Pass in a to myIncrement
-    myIncrement(a);
-    personFunctions(); // Uncomment this once you have completed the definitions of the Person class.
+
+    //personFunctions(); // Uncomment this once you have completed the definitions of the Person class.
 
     return 0;
 }
