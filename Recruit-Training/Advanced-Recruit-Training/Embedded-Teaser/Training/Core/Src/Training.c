8c5d9a714731bea989f18b50a6ae2426289b8ade
MacKante <114007326+MacKante@users.noreply.github.com>
2023-01-14 15:46:49 -0700

    Modified Teaser to make code cleaner

--- Recruit-Training/Advanced-Recruit-Training/Embedded-Teaser/Training/Core/Src/Training.c
+++ Recruit-Training/Advanced-Recruit-Training/Embedded-Teaser/Training/Core/Src/Training.c
@@ -5,47 +5,67 @@ void trainingTask(uint8_t* data)
 
 validData = 0b00000000;
 
-// Motor
-uint8_t motor = 0b10000000;
-uint8_t velocity = 0b01111110;
-uint8_t direction = 0b00000001;
-uint8_t vsign = 0b01000000;
+// Motor direction
+uint8_t direction = 0b01000001;
 
 // lights
 uint8_t headlights = 0b00000111;
+uint8_t head_shift = 0b00000100;
 uint8_t signal_lights = 0b00011000;
-uint8_t hazards = 0b00100000;
-uint8_t brakes = 0b01000000;
 
 
 // Test for motor, first two if statements checks if motors are in sync
+// Valid values for direction is 64 (0b01000000) and 1 (0b00000001)
 
-if (data[0]&motor == data[1]&motor){
-    if (data[0]&velocity == data[1]&velocity)
-    {
-        // Test for motor1 direction
-        if ((( data[0]&vsign == 0) && (data[0]&direction == 0b00000001)) || ((data[0]&vsign == 0b01000000) && (data[0]&direction == 0)))
-        {
-            outputArray[0] = data[0];
-            validData | 0b00000001;
-        }
-        else{
-        // Test for motor2 direction
-        if ((( data[1]&vsign == 0) && (data[1]&direction == 0b00000001)) || ((data[1]&vsign == 0b01000000) && (data[1]&direction == 0)))
-        {
-            outputArray[1] = data[1];
-            validData | 0b00000010;
-        }
-        }
+if (data[0] == data[1]){
+    if (data[0] & direction == 1 || data[0] & direction == 64){
+        outputArray[0] = data[0];
+        outputArray[1] = data[1];
+        validData | 0b00000011;
     }
+
 }
 
 // Test for lights
-if (data[2]&headlights == 1 || data[2]&headlights == 2 || data[2]&headlights == 4){
-    if (data[2]&signal_lights == 8 || data[2]&signal_lights == 16 || data[2]&signal_lights == 0){
+int i = 0;
+while(i < 3){
+    if ((data[2] & headlights) == (head_shift >> i)){
+        if ((data[2] & signal_lights) != 0b00011000){
+            outputArray[2] = data[2];
+            validData | 0b00000100;
+        }
+    }
+    else
+        i++;
+}
+
+
+
+
+/*   old work for motors
+if (data[0] & motor == data[1] & motor){                            // checks if both motors are on/off
+    if (data[0] & velocity == data[1] & velocity){                  // checks if both motors have same velocity
+
+        // Test for motor1 direction
+        if ((( data[0]&vsign == 0) && (data[0]&direction == 0b00000001)) || ((data[0]&vsign == 0b01000000) && (data[0]&direction == 0)))
+            outputArray[0] = data[0];
+            validData | 0b00000001;
+
+
+        // Test for motor2 direction
+        if ( (( data[1]&vsign == 0) && (data[1]&direction == 0b00000001)) || ((data[1]&vsign == 0b01000000) && (data[1]&direction == 0)) )
+            outputArray[1] = data[1];
+            validData | 0b00000010;
+    }
+}
+*/
+
+
+/*   old work for lights      
+if (data[2]&headlights == 1 || data[2]&headlights == 2 || data[2]&headlights == 4){                     // checks if only 1 headlight setting is on
+    if (data[2]&signal_lights == 8 || data[2]&signal_lights == 16 || data[2]&signal_lights == 0){       // checks if only 1 signal light setting is on
         outputArray[2] = data[2];
         validData | 0b00000100;
     }
 }
-
-
+*/
