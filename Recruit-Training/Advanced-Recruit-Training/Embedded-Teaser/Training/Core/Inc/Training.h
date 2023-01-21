19299c846658cd24346bece2fde1e59c60a61869
ishahaider <isha.haider213@gmail.com>
2023-01-21 13:14:42 -0700

    Required Commit for Update

--- Recruit-Training/Advanced-Recruit-Training/Embedded-Teaser/Training/Core/Inc/Training.h
+++ Recruit-Training/Advanced-Recruit-Training/Embedded-Teaser/Training/Core/Inc/Training.h
@@ -1,7 +1,7 @@
 #pragma once
 #include <stdint.h>
 
-uint8_t outputArray[3];
+uint8_t outputArray[3]; //uint8_t means only 8-bit integers
 uint8_t validData; // Set bits 0, 1, 2 if the input array elements were valid
-
+//validdata tells you if motor1, motor2, and lights are correct (1-correct, 0 -incorrect), bit 0 (far right) is motor1
 void trainingTask(uint8_t* data); // data should be 3 elements wide (size of 3)
