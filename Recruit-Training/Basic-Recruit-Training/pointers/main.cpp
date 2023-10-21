49d8d38982f702bf80cd5974dfa180c1ad7589ac
alexwhelan12 <alexwh2005.12@gmail.com>
2023-10-21 12:47:38 -0600

    added predicitons for p and &x

--- Recruit-Training/Basic-Recruit-Training/pointers/main.cpp
+++ Recruit-Training/Basic-Recruit-Training/pointers/main.cpp
@@ -12,6 +12,8 @@ using namespace std;
 void foo(int *a, int b);
 void bar(int *a, int b);
 
+void mySwap(int * a, int * b);
+
 int main()
 {
     int x = 3;
@@ -19,29 +21,36 @@ int main()
     int *p = &x;
 
     //TODO in the line below predict what what is going to be output
-    cout << "Predicted value of p: " /* << PLACE YOUR PREDICTION HERE*/ << endl;
+    cout << "Predicted value of p: "  << "Address of x" << endl;
     cout << "Actual value of p: " << p << endl;
-    cout << "Predicted value &x: " /* << PLACE YOUR PREDICTION HERE*/ << endl;
+    cout << "Predicted value &x: " << "Address of x" << endl;
     cout << "Actual value &x: " << &x << endl;
-    cout << "Predicted value of *p: " /* << PLACE YOUR PREDICTION HERE*/ << endl;
+    cout << "Predicted value of *p: "  << 3 << endl;
     cout << "Actual value of *p: " << *p << endl;
     
     foo(p, x);
     
-    cout << "Predicted value of *p: " /* << PLACE YOUR PREDICTION HERE*/ << endl;
+    cout << "Predicted value of *p: " << 42 << endl;
     cout << "Actual value of *p: " << *p << endl;
-    cout << "Predicted value of x: " /* << PLACE YOUR PREDICTION HERE*/ << endl;
+    cout << "Predicted value of x: "  << 42 << endl;
     cout << "Actual value of x: " << x << endl;
     foo(p, y);
 
-    cout << "Predicted value of *p: " /* << PLACE YOUR PREDICTION HERE*/ << endl;
+    cout << "Predicted value of *p: " << 365 << endl;
     cout << "Actual value of *p: " << *p << endl;
-    cout << "Predicted value of y: " /* << PLACE YOUR PREDICTION HERE*/ << endl;
+    cout << "Predicted value of y: " << 4 << endl;
     cout << "Actual value of y: " << y << endl;
 
     //start writing mySwap here
+    mySwap(p,&y);
     
     return 0;
+}
+
+void mySwap(int * a, int * b){
+    int temp = *a;
+    *a = *b;
+    *b = temp;
 }
 
  void foo(int *a, int b)
