9d4d5a3d5f4ca11431a27b56c1deacc301267359
alexwhelan12 <alexwh2005.12@gmail.com>
2023-10-21 12:46:13 -0600

    implemented person.cpp to main.cpp

--- Recruit-Training/Basic-Recruit-Training/person/main.cpp
+++ Recruit-Training/Basic-Recruit-Training/person/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <sstream>
 #include <string>
-
+#include "Person.cpp"
 #include "Person.h"
 
 void compareAnswers(const std::string myAnswer, const std::string correctAnswer)
