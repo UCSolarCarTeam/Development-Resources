cb87b9cdd70ce90651db60f268157210ce28d067
Alend-M <alend.maci@ucalgary.ca>
2023-10-28 11:55:57 -0600

    made validity status checker

--- Recruit-Training/Advanced-Recruit-Training/Embedded-Teaser/Training/Core/Src/Training.c
+++ Recruit-Training/Advanced-Recruit-Training/Embedded-Teaser/Training/Core/Src/Training.c
@@ -17,7 +17,17 @@ void trainingTask(uint8_t* data)
     if (headlightsStatus > 0){
         int headlightlevel = (((data[2] >> 6) & 0b01) == 0) ? 1:0;
     }
-    int indicatorLStatus = ((data[2] >> 4 & 0b0001) == 1)? 1:0;
+    int Rsignal = (data[2] >> 4) & 0b0001;
+    int Lsignal = (data[2] >> 3) & 0b00001;
     int hazardsStatus = (data[2] >> 2) &0b000001; 
     int brakesStatus = (data[2]>> 1) & 0b0000001;
+
+    if (statusM1 == 1 && statusM2 == 1){ //Both motors are on
+        if(velocityM1 > 0 && velocityM2 > 0 && directionM1 == directionM2){ //velocity + Direction is + 
+            if( Rsignal != Lsignal || (Lsignal == 0 && Rsignal == 0)){
+                
+            }
+        }
+    }
+
 }
