d61c7bd72c6e1b53b4f80ddc81e099459f0c341b
alexwhelan12 <alexwh2005.12@gmail.com>
2023-10-28 14:21:25 -0600

    make file didn't work

--- Recruit-Training/Advanced-Recruit-Training/Embedded-Teaser/Training/Test/TrainingTest.c
+++ Recruit-Training/Advanced-Recruit-Training/Embedded-Teaser/Training/Test/TrainingTest.c
@@ -5,24 +5,29 @@
 void runTrainingTests()
 {
     // Run all the tests here using RUN_TEST()
+    RUN_TEST();
 }
 
 void test_EverythingValid()
 {
-
+    uint8_t data[3] = {0b10000011, 0b1000000011, 0b11010010};
+    trainingTaksk(&data);
 }
 
 void test_EverythingInvalid()
 {
-
+    uint8_t data[3] = {0b10000010, 0b1000000011, 0b11011010};
+    trainingTaksk(&data);
 }
 
 void test_OnlyLightsInvalid()
 {
-
+    uint8_t data[3] = {0b10000011, 0b1000000011, 0b11011010};
+    trainingTaksk(&data);
 }
 
 void test_OnlyMotorsInvalid()
 {
-
+    uint8_t data[3] = {0b10000010, 0b1000000011, 0b11010010};
+    trainingTaksk(&data);
 }
