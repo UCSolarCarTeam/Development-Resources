f1c72f8e2e456372ab6d8bb169bb4eb594775c6e
Alend-M <alend.maci@ucalgary.ca>
2023-10-28 14:19:00 -0600

    Makefile wasnt working. Save point before able to run unit test

--- Recruit-Training/Advanced-Recruit-Training/Embedded-Teaser/Training/Test/TrainingTest.c
+++ Recruit-Training/Advanced-Recruit-Training/Embedded-Teaser/Training/Test/TrainingTest.c
@@ -5,24 +5,29 @@
 void runTrainingTests()
 {
     // Run all the tests here using RUN_TEST()
+    RUN_TEST(test_EverythingValid);
 }
 
 void test_EverythingValid()
 {
-
+    uint8_t data[3] = {0b10000111,0b10000111,0b11010010 };
+    trainingTask(&data);
 }
 
 void test_EverythingInvalid()
 {
-
+    uint8_t data[3] = {0b10000110,0b10000111,0b11011010 };
+    trainingTask(&data);
 }
 
 void test_OnlyLightsInvalid()
 {
-
+    uint8_t data[3] = {0b10000111,0b10000111,0b11011010 };
+    trainingTask(&data);
 }
 
 void test_OnlyMotorsInvalid()
 {
-
+    uint8_t data[3] = {0b10000110,0b10000111,0b11010010 };
+    trainingTask(&data);
 }
