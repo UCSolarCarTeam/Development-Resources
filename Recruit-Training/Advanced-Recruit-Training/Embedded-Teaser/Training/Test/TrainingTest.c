f603f508926d1592f695f34cf7aad728e6dde8c7
CarrotBRRR <69835046+CarrotBRRR@users.noreply.github.com>
2023-11-01 18:57:46 -0600

    Implemented Tests and Added Checks for special invalid data cases

--- Recruit-Training/Advanced-Recruit-Training/Embedded-Teaser/Training/Test/TrainingTest.c
+++ Recruit-Training/Advanced-Recruit-Training/Embedded-Teaser/Training/Test/TrainingTest.c
@@ -4,25 +4,34 @@
 
 void runTrainingTests()
 {
-    // Run all the tests here using RUN_TEST()
+    UNITY_BEGIN();
+    RUN_TEST(test_EverythingIsValid());
+    RUN_TEST(test_EverythingIsInvalid());
+    RUN_TEST(test_OnlyLightsIsInvalid());
+    RUN_TEST(test_MotorsAreInvalid());
+    return UNITY_END();
 }
 
 void test_EverythingValid()
 {
-
+    uint8_t data[3] = {0b10000001, 0b100000001, 0b1111001};
+    trainingTask(data);
 }
 
 void test_EverythingInvalid()
 {
-
+    uint8_t data[3] = {0b10000001, 0b000000001, 0b1111111};
+    trainingTask(data);
 }
 
 void test_OnlyLightsInvalid()
 {
-
+    uint8_t data[3] = {0b10000001, 0b100000001, 0b1111111};
+    trainingTask(data);
 }
 
 void test_OnlyMotorsInvalid()
 {
-
+    uint8_t data[3] = {0b10100000, 0b10100000, 0b1111001};
+    trainingTask(data);
 }
