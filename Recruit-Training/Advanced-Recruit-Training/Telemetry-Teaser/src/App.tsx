08d7ed425fdcafc616e0ae0a6fac3ee0fedec5a2
SpencerVR1 <spencer.vanroessel@ucalgary.ca>
2023-11-04 12:43:16 -0600

    added calculate button

--- Recruit-Training/Advanced-Recruit-Training/Telemetry-Teaser/src/App.tsx
+++ Recruit-Training/Advanced-Recruit-Training/Telemetry-Teaser/src/App.tsx
@@ -21,6 +21,8 @@ const App = () => {
             <WeatherInput />
           </div>
         </form>
+        <button className="bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded" onClick={calculateRange}>Calculate</button>
+        
       </div>
     </div>
   );
