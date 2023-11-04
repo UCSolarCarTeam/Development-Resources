c66901367c937f2d47574110f673e2e875c5fc0e
Alend-M <alend.maci@ucalgary.ca>
2023-11-04 15:47:41 -0600

    Everything is working except the slider

--- Recruit-Training/Advanced-Recruit-Training/Telemetry-Teaser/src/App.tsx
+++ Recruit-Training/Advanced-Recruit-Training/Telemetry-Teaser/src/App.tsx
@@ -1,28 +1,38 @@
 import { useState } from "react";
+
 import BatteryInput from "~/components/batteryInput";
 import Header from "~/components/header";
 import SpeedInput from "~/components/speedInput";
 import WeatherInput from "~/components/weatherInput";
 
 const App = () => {
-  const [s, setSpeed]  = useState(0);
+  const [s, setSpeed] = useState(0);
   const [b, setBattery] = useState(0);
-  const[w, setWeather] = useState(0);
-  const range = -(s * s * b / 2500) + (4 * b) + w;
+  const [w, setWeather] = useState(0);
+  const range = -((s * s * b) / 2500) + 4 * b + w;
   const [output, setOutput] = useState<string>("");
 
   const calculateRange = (range: any) => {
-    if(!isNaN(s)){
-      if(!isNaN(b)){
-        setOutput(range);
-      }else{
-        setOutput("*Required Battery %*");
+    if (!isNaN(s)) {
+      if (s >= 0 && s <= 90) {
+        if (!isNaN(b)) {
+          if (b >= 0 && b <= 100) {
+            setOutput("The predicted range of the Eylsia is "+ range+"km");
+          } else {
+            setOutput(
+              "The battery percentage should be with the range of 0 to 100",
+            );
+          }
+        } else {
+          setOutput("*Required Battery %*");
+        }
+      } else {
+        setOutput("The speed should be with the range of 0 to 90");
       }
-    }else{
+    } else {
       setOutput("*Required Speed*");
     }
-    
-  }
+  };
 
   return (
     <div className="h-screen w-screen bg-[#212121]">
@@ -30,15 +40,24 @@ const App = () => {
         <Header />
         <form name="simulator" className="flex w-full flex-col items-center">
           <div className="mb-4 flex w-full flex-col items-center gap-y-4">
-            <SpeedInput setSpeed={setSpeed}speed={s} />
-            <BatteryInput setBattery = {setBattery}battery={b}/>
+            <SpeedInput setSpeed={setSpeed} speed={s} />
+            <BatteryInput setBattery={setBattery} battery={b} />
           </div>
           <div className="flex w-full flex-row justify-center gap-4">
-            <WeatherInput setWeather={setWeather} />  
+            <WeatherInput setWeather={setWeather} />
           </div>
         </form>
-        <button className="bg-blue-500 text-white rounded-md py-2 px-4 text-center" onClick={()=>{calculateRange(range)}} >Calculate</button>
-        <h1 className="py-2" id = "outputT">{output}</h1>
+        <button
+          className="rounded-md bg-blue-500 px-4 py-2 text-center text-white"
+          onClick={() => {
+            calculateRange(range);
+          }}
+        >
+          Calculate
+        </button>
+        <h1 className="py-2" id="outputT">
+          {output}
+        </h1>
       </div>
     </div>
   );
