3dfd0c1a465940fd5eb73f04821a36cdea1559ca
Paula Amaya <paula.amaya@ucalgary.ca>
2023-11-04 15:16:57 -0600

    Added button with no logic

--- Recruit-Training/Advanced-Recruit-Training/Telemetry-Teaser/src/App.tsx
+++ Recruit-Training/Advanced-Recruit-Training/Telemetry-Teaser/src/App.tsx
@@ -2,6 +2,7 @@ import BatteryInput from "~/components/batteryInput";
 import Header from "~/components/header";
 import SpeedInput from "~/components/speedInput";
 import WeatherInput from "~/components/weatherInput";
+import CalculateButton from "~/components/calculateButton";
 
 const App = () => {
   const calculateRange = () => {
@@ -12,13 +13,16 @@ const App = () => {
     <div className="h-screen w-screen bg-[#212121]">
       <div className="flex h-full flex-col items-center pt-36 text-white">
         <Header />
-        <form name="simulator" className="flex w-full flex-col items-center">
+        <form name="simulator" className="flex w-full flex-col items-center bg-[#212121]">
           <div className="mb-4 flex w-full flex-col items-center gap-y-4">
             <SpeedInput />
             <BatteryInput />
           </div>
           <div className="flex w-full flex-row justify-center gap-4">
             <WeatherInput />
+          </div>
+          <div className="flex w-full flex-row justify-center gap-4 mb-4">
+            <CalculateButton />
           </div>
         </form>
       </div>
---
+++ Recruit-Training/Advanced-Recruit-Training/Telemetry-Teaser/src/components/calculateButton/index.tsx
@@ -0,0 +1,11 @@
+const CalculateButton = () => {
+  return (
+    <>
+      <div className="flex w-full flex-col items-center gap-2">
+        <input type="button" value="Calculate" className="bg-sky-900 mx-[8px] w-1/4 rounded-md px-[20px] py-[12px]"/>
+      </div>
+    </>
+  );
+};
+
+export default CalculateButton;
