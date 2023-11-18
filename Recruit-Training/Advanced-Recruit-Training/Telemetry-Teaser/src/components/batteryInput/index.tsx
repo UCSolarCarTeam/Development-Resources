d4893fb735d92a5dcfe99dba0634bf37c4f97145
alexwhelan12 <alexwh2005.12@gmail.com>
2023-11-18 12:02:12 -0700

    created props for battery, weather and speed inputs.

--- Recruit-Training/Advanced-Recruit-Training/Telemetry-Teaser/src/components/batteryInput/index.tsx
+++ Recruit-Training/Advanced-Recruit-Training/Telemetry-Teaser/src/components/batteryInput/index.tsx
@@ -1,25 +1,26 @@
 interface Props {
-    setBattery: React.Dispatch<React.SetStateAction<number>>;
-    battery: number; 
+  setBattery: React.Dispatch<React.SetStateAction<number>>;
+  battery: number;
 }
 
-
-
-const BatteryInput = (props: {setBattery:  React.Dispatch<React.SetStateAction<number>>, battery:number}) => {
+const BatteryInput = (props: {
+  setBattery: React.Dispatch<React.SetStateAction<number>>;
+  battery: number;
+}) => {
   const handleBatteryChange = (newValue: number) => {
     props.setBattery(newValue);
-  }
+  };
   return (
     <div className="flex w-full flex-col items-center gap-2">
       <label>Battery Percentage (%):</label>
       <input
-        value = {props.battery}
+        value={props.battery}
         id="battery"
         className="mx-[8px] w-1/4 rounded-md border border-solid border-[#ccc] px-[20px] py-[12px] text-black"
         name="battery"
         type="number"
         placeholder="Battery"
-        onChange ={(e) => {
+        onChange={(e) => {
           handleBatteryChange(parseInt(e.target.value));
         }}
       />
--- Recruit-Training/Advanced-Recruit-Training/Telemetry-Teaser/src/components/speedInput/index.tsx
+++ Recruit-Training/Advanced-Recruit-Training/Telemetry-Teaser/src/components/speedInput/index.tsx
@@ -3,24 +3,25 @@ interface Props {
   speed: number;
 }
 
-
-
-const SpeedInput = (props: {setSpeed: React.Dispatch<React.SetStateAction<number>>,speed:number}) => {
-  const handleSpeedChange = (newValue: number) =>{
+const SpeedInput = (props: {
+  setSpeed: React.Dispatch<React.SetStateAction<number>>;
+  speed: number;
+}) => {
+  const handleSpeedChange = (newValue: number) => {
     props.setSpeed(newValue);
-  }
+  };
   return (
     <>
       <div className="flex w-full flex-col items-center gap-2">
         <label>Speed (km/h):</label>
         <input
-          value = {props.speed}
+          value={props.speed}
           id="speed"
           className="mx-[8px] w-1/4 rounded-md border border-solid border-[#ccc] px-[20px] py-[12px] text-black"
           name="speed"
           type="number"
           placeholder="Speed"
-          onChange ={(e) => {
+          onChange={(e) => {
             handleSpeedChange(parseInt(e.target.value));
           }}
         />
