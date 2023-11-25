1f260811e01c4efd82f0715d1c6cd11c816d6371
jenniferja <jenniferjaystar2@gmail.com>
2023-11-25 12:54:57 -0700

    implemented tests
    not done yet

--- Recruit-Training/Advanced-Recruit-Training/Embedded-Teaser/Training/Core/Src/Training.c
+++ Recruit-Training/Advanced-Recruit-Training/Embedded-Teaser/Training/Core/Src/Training.c
@@ -3,7 +3,29 @@
 uint8_t outputArray[3];
 uint8_t validData; // Set bits 0, 1, 2 if the input array elements were valid
 
-void trainingTask(uint8_t* data)
-{
+void trainingTask(uint8_t* data) {
+    // Check if Motor 1 and Motor 2 are in sync (both on/off and direction)
+    if ((data[0] & 0x81) == 0x81 && (data[1] & 0x81) == 0x81) {
+        // Motor 1 and Motor 2 are in sync and valid, set bit 0 in validData
+        validData |= 0x01;
 
+        // Set the output array values to the input array values for Motor 1 and Motor 2
+        outputArray[0] = data[0];
+        outputArray[1] = data[1];
+    }
+
+    // Check if Lights data is valid
+    if ((data[2] & 0b00011111) == data[2]) {
+        // Lights data is valid, set bit 1 in validData
+        validData |= 0x02;
+
+        // Set the output array value to the input array value for Lights
+        outputArray[2] = data[2];
+    }
+
+    // Check if any of the motors are on (bit 7)
+    if ((data[0] & 0x80) || (data[1] & 0x80)) {
+        // At least one motor is on, set bit 2 in validData
+        validData |= 0x04;
+    }
 }
--- Recruit-Training/Advanced-Recruit-Training/Embedded-Teaser/Training/Test/TrainingTest.c
+++ Recruit-Training/Advanced-Recruit-Training/Embedded-Teaser/Training/Test/TrainingTest.c
@@ -7,25 +7,94 @@ extern uint8_t validData;
 
 void runTrainingTests()
 {
-    // Run all the tests here using RUN_TEST()
+    RUN_TEST(test_EverythingValid);
+    RUN_TEST(test_EverythingInvalid);
+    RUN_TEST(test_OnlyLightsInvalid);
+    RUN_TEST(test_OnlyMotorsInvalid);
+
 }
 
 void test_EverythingValid()
 {
+    // Test case: Everything is valid
+    uint8_t data[3] = {0x8F, 0x8F, 0x3F};  // Motor 1, Motor 2, Lights are all valid
 
+    // Initialize the output array and validData
+    outputArray[0] = 0;
+    outputArray[1] = 0;
+    outputArray[2] = 0;
+    validData = 0;
+
+    // Call the trainingTask function with the input data
+    trainingTask(data);
+
+    // Assert that all components are valid
+    TEST_ASSERT_EQUAL_HEX8(0x8F, outputArray[0]); // Motor 1
+    TEST_ASSERT_EQUAL_HEX8(0x8F, outputArray[1]); // Motor 2
+    TEST_ASSERT_EQUAL_HEX8(0x3F, outputArray[2]); // Lights
+    TEST_ASSERT_EQUAL_HEX8(0x07, validData);      // All components are valid
 }
 
 void test_EverythingInvalid()
 {
+    // Test case: Everything is invalid
+    uint8_t data[3] = {0x00, 0x00, 0xE0};  // Motor 1, Motor 2, Lights are all invalid
 
+    // Initialize the output array and validData
+    outputArray[0] = 0;
+    outputArray[1] = 0;
+    outputArray[2] = 0;
+    validData = 0;
+
+    // Call the trainingTask function with the input data
+    trainingTask(data);
+
+    // Assert that all components are invalid
+    TEST_ASSERT_EQUAL_HEX8(0x00, outputArray[0]); // Motor 1
+    TEST_ASSERT_EQUAL_HEX8(0x00, outputArray[1]); // Motor 2
+    TEST_ASSERT_EQUAL_HEX8(0x00, outputArray[2]); // Lights
+    TEST_ASSERT_EQUAL_HEX8(0x00, validData);      // All components are invalid
 }
 
 void test_OnlyLightsInvalid()
 {
+    // Test case: Only Lights is invalid
+    uint8_t data[3] = {0x8F, 0x8F, 0xE0};  // Motor 1, Motor 2 are valid, Lights is invalid
+
+    // Initialize the output array and validData
+    outputArray[0] = 0;
+    outputArray[1] = 0;
+    outputArray[2] = 0;
+    validData = 0;
+
+    // Call the trainingTask function with the input data
+    trainingTask(data);
+
+    // Assert that only Lights is invalid
+    TEST_ASSERT_EQUAL_HEX8(0x8F, outputArray[0]); // Motor 1
+    TEST_ASSERT_EQUAL_HEX8(0x8F, outputArray[1]); // Motor 2
+    TEST_ASSERT_EQUAL_HEX8(0x00, outputArray[2]); // Lights
+    TEST_ASSERT_EQUAL_HEX8(0x02, validData);      // Lights is invalid
 
 }
 
 void test_OnlyMotorsInvalid()
 {
+    // Test case: Only Motors are invalid
+    uint8_t data[3] = {0x00, 0x00, 0x3F};  // Motor 1, Motor 2 are invalid, Lights is valid
 
+    // Initialize the output array and validData
+    outputArray[0] = 0;
+    outputArray[1] = 0;
+    outputArray[2] = 0;
+    validData = 0;
+
+    // Call the trainingTask function with the input data
+    trainingTask(data);
+
+    // Assert that only Motors are invalid
+    TEST_ASSERT_EQUAL_HEX8(0x00, outputArray[0]); // Motor 1
+    TEST_ASSERT_EQUAL_HEX8(0x00, outputArray[1]); // Motor 2
+    TEST_ASSERT_EQUAL_HEX8(0x3F, outputArray[2]); // Lights
+    TEST_ASSERT_EQUAL_HEX8(0x04, validData);      // Motors are invalid
 }
