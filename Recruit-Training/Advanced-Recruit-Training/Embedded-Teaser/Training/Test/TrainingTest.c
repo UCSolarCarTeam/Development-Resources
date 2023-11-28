e5713a95176f954a0f9a1e69d27135e8b81354d5
Paula Amaya <paula.amaya@ucalgary.ca>
2023-11-27 19:04:39 -0700

    All the tests passed yayyyyyyyyyy

--- Recruit-Training/Advanced-Recruit-Training/Embedded-Teaser/Training/Test/TrainingTest.c
+++ Recruit-Training/Advanced-Recruit-Training/Embedded-Teaser/Training/Test/TrainingTest.c
@@ -11,7 +11,7 @@ void runTrainingTests()
     RUN_TEST(test_EverythingValid);
     RUN_TEST(test_EverythingInvalid);
     RUN_TEST(test_OnlyLightsInvalid);
-    // RUN_TEST(test_OnlyMotorsInvalid);
+    RUN_TEST(test_OnlyMotorsInvalid);
 }
 
 void test_EverythingValid()
@@ -46,12 +46,14 @@ void test_OnlyLightsInvalid()
     TEST_ASSERT_EQUAL_UINT8_ARRAY(expected, outputArray, 3);
 
     // Case 2: Hazards off, but both signals are on
+    resetOutput();
     uint8_t data2[3] = {0b10010111,0b10010111,0b10011000};
     trainingTask(data2);
     TEST_ASSERT_EQUAL_UINT8(0b11000000, validData);
     TEST_ASSERT_EQUAL_UINT8_ARRAY(expected, outputArray, 3);
 
     // Case 3: Hazards on, but signals not flashing
+    resetOutput();
     uint8_t data3[3] = {0b10010111,0b10010111,0b10010100};
     trainingTask(data3);
     TEST_ASSERT_EQUAL_UINT8(0b11000000, validData);
@@ -61,11 +63,20 @@ void test_OnlyLightsInvalid()
 
 void test_OnlyMotorsInvalid()
 {
+    uint8_t expected[3] = {0,0,0b01000000};
     // Case 1: Motors not in sync
+    resetOutput();
     uint8_t data1[3] = {0b11100111, 0b01001001, 0b01000000};
+    trainingTask(data1);
+    TEST_ASSERT_EQUAL_UINT8(0b00100000, validData);
+    TEST_ASSERT_EQUAL_UINT8_ARRAY(expected, outputArray, 3);
 
     // Case 2: Motors in sync, but turned off with non-zero velocity
+    resetOutput();
     uint8_t data2[3] = {0b11000000, 0b11000000, 0b01000000};
+    trainingTask(data2);
+    TEST_ASSERT_EQUAL_UINT8(0b00100000, validData);
+    TEST_ASSERT_EQUAL_UINT8_ARRAY(expected, outputArray, 3);
 }
 
 void resetOutput() {
