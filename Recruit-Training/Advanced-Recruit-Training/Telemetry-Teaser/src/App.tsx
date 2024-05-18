a9b05a56bae05870a2e136a1b3e96ac37720a991
David-Rodriguez-Barrios <david.rodriguezbarri@ucalgary.ca>
2024-05-18 15:20:43 -0600

    FIX: Reduced the number of states and implemented changes to get rid of the km display when an input changes

--- Recruit-Training/Advanced-Recruit-Training/Telemetry-Teaser/src/App.tsx
+++ Recruit-Training/Advanced-Recruit-Training/Telemetry-Teaser/src/App.tsx
@@ -6,68 +6,47 @@ import SpeedInput from "~/components/speedInput";
 import WeatherInput from "~/components/weatherInput";
 
 const App = () => {
-  const [speed, setSpeed] = useState<number | undefined>(0);
-  const [speedValid, setSpeedValid] = useState(true);
-  const [battery, setBattery] = useState<number | undefined>(100);
-  const [batteryValid, setBatteryValid] = useState(true);
-  const [weather, setWeather] = useState(50);
-  const [range, setRange] = useState<number | null>(null);
+  const [visible, setVisible] = useState(false);
+  type InputState = {
+    speed: number | undefined;
+    battery: number | undefined;
+    weather: number;
+  };
+  const [inputs, setInputs] = useState<InputState>({
+    speed: 0,
+    weather: 50,
+    battery: 100,
+  });
 
-  const calculateRange = (
-    event: React.MouseEvent<HTMLButtonElement, MouseEvent>,
-  ) => {
-    event.preventDefault();
-    if (
-      speed !== undefined &&
-      weather !== undefined &&
-      battery !== undefined &&
-      batteryValid &&
-      speedValid
-    ) {
-      const value = parseFloat(
-        (-((speed * speed * battery) / 2500) + 4 * battery + weather).toFixed(
-          3,
-        ),
-      );
-
-      setRange(value);
-    } else {
-      setRange(null);
-    }
+  const calculateRange = () => {
+    event?.preventDefault();
+    setVisible(true);
   };
 
   const BatteryChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const value = parseInt(event.target.value);
-    if (value <= 100 && value >= 0) {
-      setBattery(value);
-      setBatteryValid(true);
-    } else if (value > 100 || value < 0) {
-      setBatteryValid(false);
-      setBattery(value);
+    setVisible(false);
+    if (value) {
+      setInputs({ ...inputs, battery: value });
     } else {
-      setBattery(undefined);
-      setBatteryValid(true);
+      setInputs({ ...inputs, battery: undefined });
     }
   };
 
   const SpeedChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const value = parseInt(event.target.value);
-
-    if (value <= 90 && value >= 0) {
-      setSpeed(value);
-      setSpeedValid(true);
-    } else if (value > 90 || value < 0) {
-      setSpeedValid(false);
-      setSpeed(value);
+    setVisible(false);
+    if (value !== undefined) {
+      setInputs({ ...inputs, speed: value });
     } else {
-      setSpeed(undefined);
-      setSpeedValid(true);
+      setInputs({ ...inputs, speed: undefined });
     }
   };
 
   const sliderChange = (num: string) => {
     const value = parseFloat(num);
-    setWeather(value);
+    setInputs({ ...inputs, weather: value });
+    setVisible(false);
   };
 
   return (
@@ -76,43 +55,59 @@ const App = () => {
         <Header />
         <form name="simulator" className="flex w-full flex-col items-center">
           <div className="mb-4 flex w-full flex-col items-center gap-y-4">
-            <SpeedInput value={speed} onChange={SpeedChange} />
-            {!speedValid && (
-              <p className="text-red-500">
-                {" "}
-                The speed should be with the range of 0 to 90
-              </p>
-            )}
-            {speed === undefined && (
+            <SpeedInput value={inputs["speed"]} onChange={SpeedChange} />
+            {inputs["speed"] !== undefined &&
+              (inputs["speed"] > 100 || inputs["speed"] < 0) && (
+                <p className="text-red-500">
+                  {" "}
+                  The speed should be within the range of 0 to 90
+                </p>
+              )}
+            {inputs["speed"] === undefined && (
               <p className="text-red-500">Speed is required</p>
             )}
-            <BatteryInput value={battery} onChange={BatteryChange} />
-            {!batteryValid && (
-              <p className="text-red-500">
-                The battery percentage should be with the range of 0 to 100
-              </p>
-            )}
-            {battery === undefined && (
+            <BatteryInput value={inputs["battery"]} onChange={BatteryChange} />
+            {inputs["battery"] !== undefined &&
+              (inputs["battery"] > 100 || inputs["battery"] < 0) && (
+                <p className="text-red-500">
+                  The battery percentage should be with the range of 0 to 100
+                </p>
+              )}
+            {inputs["battery"] === undefined && (
               <p className="text-red-500">Battery percentage is required</p>
             )}
           </div>
           <div className="flex w-full flex-row justify-center gap-4">
-            <WeatherInput value={weather} onChange={sliderChange} />
+            <WeatherInput value={inputs["weather"]} onChange={sliderChange} />
           </div>
 
           <button
             className="mt-10 h-10 w-60 rounded bg-blue-200 text-white"
-            onClick={(event: React.MouseEvent<HTMLButtonElement, MouseEvent>) =>
-              calculateRange(event)
-            }
+            onClick={() => calculateRange()}
           >
             Submit
           </button>
-          {range && (
-            <p className="mt-10">
-              The predicted range of the Eylsia is {range} km
-            </p>
-          )}
+          {visible &&
+            inputs["speed"] !== undefined &&
+            inputs["battery"] !== undefined &&
+            inputs["weather"] !== undefined &&
+            inputs["battery"] <= 100 &&
+            inputs["battery"] >= 0 &&
+            inputs["speed"] <= 100 &&
+            inputs["speed"] >= 0 && (
+              <p className="mt-10">
+                The predicted range of the Eylsia is{" "}
+                {(
+                  -(
+                    (inputs["speed"] * inputs["speed"] * inputs["battery"]) /
+                    2500
+                  ) +
+                  4 * inputs["battery"] +
+                  inputs["weather"]
+                ).toFixed(3)}{" "}
+                km
+              </p>
+            )}
         </form>
       </div>
     </div>
