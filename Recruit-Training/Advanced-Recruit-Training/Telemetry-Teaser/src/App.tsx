80eb280a7b227901ca781ccd85dba297665d7212
zainab_majid <zainabmajid2408@gmail.com>
2024-10-01 18:30:11 -0600

    created state variables to st ore input values

--- Recruit-Training/Advanced-Recruit-Training/Telemetry-Teaser/src/App.tsx
+++ Recruit-Training/Advanced-Recruit-Training/Telemetry-Teaser/src/App.tsx
@@ -2,9 +2,18 @@ import BatteryInput from "~/components/batteryInput";
 import Header from "~/components/header";
 import SpeedInput from "~/components/speedInput";
 import WeatherInput from "~/components/weatherInput";
+import React, { useState } from "react";
 
 const App = () => {
+  const[speed, setSpeed] = useState(0);
+  const[battery, setBattery] = useState(0);
+  const[weather, setWeather] = useState(0);
+  const[range, setRange] = useState(0);
+  
   const calculateRange = () => {
+    //range = -(s * s * b / 2500) + (4 * b) + w
+    //Where s = speed, b = battery percentage w = weather 
+    
     return;
   }
 
@@ -29,6 +38,7 @@ const App = () => {
             >
               Calculate</button>
           </div>
+
         </form>
       </div>
     </div>
