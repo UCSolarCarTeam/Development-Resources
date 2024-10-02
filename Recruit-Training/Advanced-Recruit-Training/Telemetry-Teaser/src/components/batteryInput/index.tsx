5c3ef5927cb29dace12e49ab49e4be5c2eab954a
zainab_majid <zainabmajid2408@gmail.com>
2024-10-01 20:19:50 -0600

    displaying the result

--- Recruit-Training/Advanced-Recruit-Training/Telemetry-Teaser/src/components/batteryInput/index.tsx
+++ Recruit-Training/Advanced-Recruit-Training/Telemetry-Teaser/src/components/batteryInput/index.tsx
@@ -1,4 +1,9 @@
-const BatteryInput = () => {
+interface BatteryInputProps {
+  value: number | string; // Expecting a string for the input value
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void; // Function to handle changes
+}
+
+const BatteryInput: React.FC<BatteryInputProps> = ({ value, onChange }) => {
   return (
     <div className="flex w-full flex-col items-center gap-2">
       <label>Battery Percentage (%):</label>
@@ -8,6 +13,9 @@ const BatteryInput = () => {
         name="battery"
         type="number"
         placeholder="Battery"
+        value={value}
+        onChange={onChange}
+        required
       />
     </div>
   );
