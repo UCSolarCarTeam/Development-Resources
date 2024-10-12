9d428e669d76f6bf346728882448e76ee76e8082
sim <simarkandola51@gmail.com>
2024-10-12 15:36:54 -0600

    struggling

--- Recruit-Training/Basic-Telemetry-Training/src/App.jsx
+++ Recruit-Training/Basic-Telemetry-Training/src/App.jsx
@@ -9,15 +9,16 @@ function App() {
       <div className="flex justify-center items-center h-screen">
         <div className="box-border h-1/2 w-1/5 bg-pink-200 rounded-lg">
           <div className="box-border h-1/5 w-5/6 bg-pink-300 rounded-lg m-auto mt-4"></div>
-          <div>
+          <div class="h-56 grid grid-cols-3 gap-4 content-start ..." id = "buttons">
+            <Button />
+            <Button />
+            <Button />
+            <Button />
+            <Button />
             <Button />
           </div>
+          </div>
         </div>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <Button/>
     </>
   );
 }
