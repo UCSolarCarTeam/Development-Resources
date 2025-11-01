e024e6513e227de24d4184b6c340491d617eaf7a
VyapakBansal <167020115+VyapakBansal@users.noreply.github.com>
2025-11-01 10:45:50 -0600

    3rd Try

--- Recruit-Training/Telemetry-Recruit-Training/first-training/src/App.jsx
+++ Recruit-Training/Telemetry-Recruit-Training/first-training/src/App.jsx
@@ -9,35 +9,28 @@ import "./App.css";
  https://cdn.i-scmp.com/sites/default/files/styles/1020x680/public/images/methode/2018/05/23/b70f3e2e-5d99-11e8-a4de-9f5e0e4dd719_1280x720_095446.JPG?itok=b7E9r0SX
  https://cdn.britannica.com/52/170952-050-A545E35D/carnivore-Snow-leopard-regions-subcontinent-Asia-Indian.jpg
  */
+
+
 function App() {
+  const images = [
+    {name: "Leopard 1", link: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Irbis4.JPG/1200px-Irbis4.JPG", alt: "Leopard 1"},
+    {name: "Leopard 2", link: "https://files.worldwildlife.org/wwfcmsprod/images/Snow_Leopard_hero_species_2021/hero_small/8hwbyi3z8p_species_snowleopard_hero.jpg", alt: "Leopard 2"},
+    {name: "Leopard 3", link: "https://images.saymedia-content.com/.image/t_share/MTc2NDYyMTcxMDg4Mjk5OTk0/the-endangered-snow-leopard.jpg", alt: "Leopard 3"},
+    {name: "Leopard 4", link: "https://cdn.hswstatic.com/gif/gettyimages-1789936680.jpg", alt: "Leopard 4"},
+    {name: "Leopard 5", link: "https://cdn.i-scmp.com/sites/default/files/styles/1020x680/public/images/methode/2018/05/23/b70f3e2e-5d99-11e8-a4de-9f5e0e4dd719_1280x720_095446.JPG?itok=b7E9r0SX", alt: "Leopard 5"},
+    {name: "Leopard 6", link: "https://cdn.britannica.com/52/170952-050-A545E35D/carnivore-Snow-leopard-regions-subcontinent-Asia-Indian.jpg", alt: "Leopard 6"},
+  ]
+
   return (
     <>
       <h1>Snow Leopard Photos</h1>
       <div className="Container">
-        <div className="title">
-          <h4>Leopard 1</h4>
-          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Irbis4.JPG/1200px-Irbis4.JPG" alt="1st one"/>
-        </div>
-        <div className="title">
-          <h4>Leopard 2</h4>
-          <img src="https://files.worldwildlife.org/wwfcmsprod/images/Snow_Leopard_hero_species_2021/hero_small/8hwbyi3z8p_species_snowleopard_hero.jpg" alt="2nd one"/>
-        </div>
-        <div className="title">
-          <h4>Leopard 3</h4>
-          <img src="https://images.saymedia-content.com/.image/t_share/MTc2NDYyMTcxMDg4Mjk5OTk0/the-endangered-snow-leopard.jpg" alt="3rd one"/>
-        </div>
-        <div className="title">
-          <h4>Leopard 4</h4>
-          <img src="https://cdn.hswstatic.com/gif/gettyimages-1789936680.jpg" alt="4th one"/>
-        </div>
-        <div className="title">
-          <h4>Leopard 5</h4>
-          <img src="https://cdn.i-scmp.com/sites/default/files/styles/1020x680/public/images/methode/2018/05/23/b70f3e2e-5d99-11e8-a4de-9f5e0e4dd719_1280x720_095446.JPG?itok=b7E9r0SX" alt="5th one"/>
-        </div>
-        <div className="title">
-          <h4>Leopard 6</h4>
-          <img src="https://cdn.britannica.com/52/170952-050-A545E35D/carnivore-Snow-leopard-regions-subcontinent-Asia-Indian.jpg" alt="6th one"/>
-        </div>
+        {images.map((image) => (
+          <div className="title">
+            <h4>{image.name}</h4>
+            <img src={image.link} alt={image.alt}/>
+          </div>
+      ))}
       </div>
     </>
   )
